e7dd8556a557ebf43224eb863d29fee8e7672224
tunnckoCore <mameto_100@mail.bg>
2016-03-26 03:37:20 +0200

    update tests

--- test.js
+++ test.js
@@ -34,24 +34,6 @@ function three () {
 }
 var fns = [one, two, three]
 
-/**
- * Hooks
- */
-
-var results = []
-var values = []
-
-function beforeEach (val) {
-  values.push(val)
-}
-function afterEach (er, res) {
-  results.push(res)
-}
-
-/**
- * Tests
- */
-
 test('should exposed constructor be signleton', function (done) {
   test.strictEqual(typeof Ctor === 'function', true)
   test.strictEqual(typeof Ctor() === 'object', true)
@@ -65,40 +47,10 @@ test('should be event emitter', function (done) {
   done()
 })
 
-test('should emit beforeEach/afterEach and listen with `base.on` ', function (done) {
-  var app = new Ctor()
-  app.on('beforeEach', beforeEach).on('afterEach', afterEach)
-  ctrl.mapSeries(fns, app.makeIterator(), function () {
-    test.deepEqual(results, ['bar', 'baz', 'qux'])
-    test.deepEqual(values, fns)
-    done()
-  })
-})
-
-test('should async.mapSeries be settled', function (done) {
-  var iterator = base.makeIterator({settle: true})
-
-  function throwError () {
-    throw new Error('two err')
-  }
-
-  ctrl.mapSeries([
-    function one () { return 1 },
-    function two () {
-      throwError()
-      /* istanbul ignore next */
-      return 2
-    },
-    function three (cb) { cb(null, 3) }
-  ], iterator, function (err, res) {
-    test.strictEqual(err, null)
-    test.strictEqual(res.length, 3)
-    test.strictEqual(res[0], 1)
-    test.strictEqual(res[2], 3)
-    test.strictEqual(res[1] instanceof Error, true)
-    test.strictEqual(res[1].message, 'two err')
-    done()
-  })
+test('should be AsyncSimpleIterator app and have .wrapIterator method', function (done) {
+  test.strictEqual(typeof base.wrapIterator, 'function')
+  test.strictEqual(typeof base.makeIterator, 'function')
+  done()
 })
 
 test('should functions share context', function (done) {
@@ -109,38 +61,24 @@ test('should functions share context', function (done) {
   })
 })
 
-test('should listen for beforeEach events', function (done) {
-  var res = []
-  base.on('beforeEach', function (fn) {
-    res.push(fn)
-  })
-  ctrl.mapSeries(fns, base.makeIterator(), function (err) {
-    test.ifError(err)
-    test.deepEqual(res, fns)
-    done()
-  })
-})
-
-test('should be able to pass hooks through options', function (done) {
-  var functions = []
-  var results = []
-
-  function beforeEach (fn) {
-    functions.push(fn)
-  }
-  function afterEach (er, res, fn) {
-    results.push(res)
-  }
-
+test('should be able to pass params to functions through options.params', function (done) {
   var iterator = base.makeIterator({
-    beforeEach: beforeEach,
-    afterEach: afterEach
+    params: [{bar: 'qux'}, 3]
   })
-
-  ctrl.mapSeries(fns, iterator, function (err) {
+  ctrl.mapSeries([
+    function one (obj, num) {
+      test.deepEqual(obj, {bar: 'qux'})
+      test.strictEqual(num, 3)
+      return obj
+    },
+    function two (obj, num, next) {
+      test.deepEqual(obj, {bar: 'qux'})
+      test.strictEqual(num, 3)
+      next(null, num)
+    }
+  ], iterator, function (err, res) {
     test.ifError(err)
-    test.deepEqual(functions, fns)
-    test.deepEqual(results, ['bar', 'baz', 'qux'])
+    test.deepEqual(res, [{bar: 'qux'}, 3])
     done()
   })
 })
